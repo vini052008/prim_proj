881b3ffd3f7bc34e03db02b98bd74db395e754a8
vini <vinicius.tosta@escola.pr.gov.br>
2024-11-28 14:35:24 -0300

    Adiciona verificação de números negativos

--- soma.py
+++ soma.py
@@ -1,7 +1,9 @@
-# soma.py
-# Script que soma dois números
+# soma.py com verificação
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
 
-soma = num1 + num2
-print("A soma dos números é:", soma)
+if num1 < 0 or num2 < 0:
+    print("Pelo menos um número é negativo.")
+else:
+    soma = num1 + num2
+    print("A soma dos números é:", soma)
